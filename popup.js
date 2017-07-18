6fb5b8bbacc547e098ef4cd76c3a3f7b9feb57f0
Graham Seyffert <graham.seyffert@capitalone.com>
2017-07-18 11:30:14 -0700

    Script working.

--- popup.js
+++ popup.js
@@ -1,49 +1,23 @@
-
 /**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
+ * Checks if window href contains 'article' reference and removes body class to
+ * display full article + remove pagination element.
  */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
+function revealArticle() {
+  const url = window.location.href;
+  const isArticle = url.indexOf('article') > -1;
+  if (isArticle) {
+    const rootElem = document.getElementsByClassName('sa-a')[0];
+    rootElem.setAttribute('class', '');
+    removePagination();
+  }
 }
 
-// Page 1
-class="sa-a" // Remove class to view full article!
+function removePagination() {
+  setTimeout(function() {
+    const paginationElem = document.getElementById('a-pagination');
+    if (!paginationElem) return removePagination();
+    else return paginationElem.parentElement.removeChild(paginationElem);
+  }, 1000);
+}
 
-// Page 2
-class="sa-rb modal fade in" // Delete node
-class="sa-a modal-open" // Set class to ''
-
-id="a cont" class="rb" // Set class to ''
-
-// document.addEventListener('DOMContentLoaded', function() {
-  
-// });
+revealArticle();
