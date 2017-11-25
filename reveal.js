a0080cc0e6b11d3caded27ffcb61fc0c08d3d5ef
Graham Seyffert <graham.seyffert@capitalone.com>
2017-11-25 09:24:44 -0800

    Update script.

--- reveal.js
+++ reveal.js
@@ -2,14 +2,27 @@
  * Checks if window href contains 'article' reference and removes body class to
  * display full article & remove pagination element.
  */
-function revealArticle() {
+function checkIsArticle() {
   const url = window.location.href;
   const isArticle = url.indexOf('article') > -1;
   if (isArticle) {
-    const rootElem = document.getElementsByClassName('sa-a')[0];
-    rootElem.setAttribute('class', '');
-    removePagination();
+    revealArticle();
+    closeModal(0);
   }
+}
+
+/**
+ * Checks for element that hides additional pages of article and removes it's ID.
+ */
+function revealArticle() {
+  setTimeout(function() {
+    const articleElem = document.getElementById('a-cont');
+    if (!articleElem) return revealArticle();
+    else {
+      articleElem.setAttribute('id', '');
+      removePagination();
+    }
+  }, 1000);
 }
 
 /**
@@ -24,4 +37,25 @@ function removePagination() {
   }, 1000);
 }
 
-revealArticle();
+/**
+ * Checks for a login modal and removes it + makes page scrollable again if it exists.
+ * @param { number } numTries The number of times this function has been called.
+ */
+function closeModal(numTries) {
+  if (numTries > 45) return;
+  setTimeout(function() {
+    const modalElem = document.getElementsByClassName('modal');
+    if (!modalElem || modalElem.length === 0) return closeModal(numTries + 1);
+    else {
+      Array.prototype.forEach.call(modalElem, function(elem) {
+        elem.parentElement.removeChild(elem);
+      });
+      const body = document.getElementsByClassName('modal-open');
+      Array.prototype.forEach.call(body, function(elem) {
+        elem.setAttribute('class', '');
+      });
+    }
+  }, 1000);
+}
+
+checkIsArticle();
